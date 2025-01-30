a540c77f3859a1c73aa96128c83577439641cd01
Sheetal <sheetalrana190@gmail.com>
2025-01-30 12:15:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* Author: Sheetal Rana
+* Date: 30 January 2025
+* Project: Task_1
+* Description:
+* Calculate total money spent and average price per trip for Carlo's business travels.
+*/
+
+namespace AssignmentTaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
