d47d09f6344559bc825293dc5c2c7a2e5cab22c2
Sheetal <sheetalrana190@gmail.com>
2025-01-30 12:36:54 -0500

    written the code for user input to enter the number of trips committed to calgary, vancouver and montreal

--- Program.cs
+++ Program.cs
@@ -16,6 +16,15 @@ namespace AssignmentTaskOne
             int torontoToCalgary = 1350 * 2;
             int torontoToVancouver = 1500 * 2;
             int torontoToMontreal = 575 * 2;
+            // Code to enter number of trips taken
+            Console.WriteLine("Enter number of trips to Calgary:");
+            int tripsCalgary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEnter number of trips to Vancouver:");
+            int tripsVancouver = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEnter number of trips to Montreal:");
+            int tripsMontreal = int.Parse(Console.ReadLine());
 
         }
     }
